223fc196125f99bf186fcc4a0af1388c841a28b5
Tommi Kaikkonen <tommi@kaikkonendesign.fi>
2013-08-02 10:22:43 +0300

    fix south introspection rule

--- taggit_autosuggest_select2/models.py
+++ taggit_autosuggest_select2/models.py
@@ -1,6 +1,6 @@
 try:
     from south.modelsinspector import add_ignored_fields
-    add_ignored_fields(["^taggit_autosuggest\.managers"])
+    add_ignored_fields(["^taggit_autosuggest_select2\.managers"])
 except ImportError:
     pass  # without south this can fail silently
  
